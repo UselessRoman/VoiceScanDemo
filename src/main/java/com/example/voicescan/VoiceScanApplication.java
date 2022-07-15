3f4b1d4d47851006f6511d6ca548a91f21c2608f
wjc <865997286@qq.com>
2022-07-15 22:10:02 +0800

    init

---
+++ src/main/java/com/example/voicescan/VoiceScanApplication.java
@@ -0,0 +1,13 @@
+package com.example.voicescan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceScanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceScanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/voicescan/VoiceScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voicescan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceScanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
